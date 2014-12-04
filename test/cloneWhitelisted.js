36a455d92a9251019d77a6a2201007f8301536b8
André Gaul <andre@gaul.io>
2014-12-04 18:17:43 +0100

    move obj.js into cloneWhitelisted.js

--- test/cloneWhitelisted.js
+++ test/cloneWhitelisted.js
@@ -2,7 +2,16 @@ var should = require('should');
 var _ = require('underscore');
 
 var authorize = require('../');
-var obj = require('./obj');
+var obj = {
+  field1: 1,
+  field2: 'foo',
+  nested: {
+    field1: 2,
+    field2: 'bar'
+  },
+  array1: [1, 2, 3],
+  array2: [{foo: 'bar', fuzz: 'bear'}, {foo: 'baz'}]
+};
 
 describe('cloneWhitelisted()', function () {
   cloneWhitelisted = authorize.cloneWhitelisted;
