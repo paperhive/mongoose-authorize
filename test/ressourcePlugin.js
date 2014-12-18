b8dfea7d5ade7894aadf8a377fb9388970eb6e26
André Gaul <andre@gaul.io>
2014-12-18 14:56:00 +0100

    [ressource] recursively serialize (authorized) to JSON

--- test/ressourcePlugin.js
+++ test/ressourcePlugin.js
@@ -30,6 +30,7 @@ describe('ressourcePlugin', function () {
 
             // recursively process document until a subdocument is reached
             function processObj (obj, path) {
+
               // check if path is valid
               if (path && !doc.schema.paths[path] && !doc.schema.nested[path]) {
                 return;
@@ -42,13 +43,18 @@ describe('ressourcePlugin', function () {
                   if (doc.schema.paths[path].schema) {
                     return docToJSON(value, readRessources);
                   }
+                  // is this a populated referenced document?
+                  var type = doc.schema.paths[path].options.type;
+                  if (_.isArray(type) && type.length && type[0].ref) {
+                    if (!_.isFunction(value.authorizedToJSON)) {
+                      return;
+                    }
+                    return value.authorizedToJSON();
+                  }
                   // something else
                   return processObj(value, path);
                 }));
                 return array.length ? array : undefined;
-              // obj is ObjectId
-              } else if (obj instanceof mongoose.Types.ObjectId) {
-                return processObj(obj.toString(), path);
               // obj is null, string, number, boolean
               } else if (_.isNull(obj) || _.isString(obj) ||
                          _.isNumber(obj) || _.isBoolean(obj)) {
@@ -63,9 +69,19 @@ describe('ressourcePlugin', function () {
               }
               // obj is an object
               else if (_.isObject(obj) && !_.isFunction(obj)) {
-                var ret = {};
+
+                // is this a populated referenced document?
+                if (doc.schema.paths[path] &&
+                    doc.schema.paths[path].options.ref) {
+                  var popdoc = doc.get(path);
+                  if (!_.isFunction(popdoc.authorizedToJSON)) {
+                    return;
+                  }
+                  return popdoc.authorizedToJSON();
+                }
 
                 // process keys
+                var ret = {};
                 _.each(_.keys(obj), function (key) {
                   var curPath = path ? path + '.' + key : key;
 
@@ -84,7 +100,7 @@ describe('ressourcePlugin', function () {
               return;
             }
 
-            var ret = processObj(doc.toJSON());
+            var ret = processObj(doc.toJSON()); //{depopulate: true}
             if (_.isEmpty(ret)) return;
 
             // insert _id
@@ -122,7 +138,7 @@ describe('ressourcePlugin', function () {
               animation: {type: Boolean, ressource: 'settings'}
             }
           },
-          knows: {type: mongoose.Schema.Types.ObjectId, ref: 'User', ressource: 'info'}
+          knows: [{type: mongoose.Schema.Types.ObjectId, ref: 'User1', ressource: 'info'}]
         });
         userSchema.plugin(ressourcePlugin, {ressource: 'info'});
         userSchema.plugin(authorize.permissionsPlugin);
@@ -153,25 +169,40 @@ describe('ressourcePlugin', function () {
 
         var User = mongoose.model('User1', userSchema);
 
-        User.create({
-          name: 'André',
-          passwordHash: '0eaf4f4c',
-          emails: [
-            {address: 'andre@gaul.io', type: 'private', visible: true}
-          ],
-          settings: {rememberMe: true},
-          knows: user1
-        }, function (err, user) {
-          if (err) return done(err);
-          user.setPassword(user._id, 'pass', function (err) {
-            if (err) return done(err);
-            user.populate('knows', function () {
-              console.log(user.authorizedToJSON());
-              console.log(user);
-              done();
+        async.waterfall([
+          function (cb) {
+            User.create({
+              name: 'André',
+              passwordHash: '0eaf4f4c',
+              emails: [
+                {address: 'andre@gaul.io', type: 'private', visible: true}
+              ],
+              settings: {rememberMe: true},
+              knows: user1
+            }, {
+              name: 'Schlömi',
+              settings: {rememberMe: false}
+            }, cb);
+          },
+          function (andre, schloemi, cb) {
+            andre.knows = [schloemi];
+            andre.save(cb);
+          },
+          function (andre, _, cb) {
+            andre.setPassword(andre._id, 'doener', function (err) {
+              if (err) return cb(err);
+              cb(null, andre);
             });
-          });
-        });
+          },
+          function (andre, cb) {
+            andre.populate('knows', cb);
+          },
+          function (andre, cb) {
+            console.log(andre.authorizedToJSON());
+            console.log(andre);
+            cb();
+          }
+        ], done);
       });
     });
   });
